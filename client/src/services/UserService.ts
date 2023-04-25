b1780de1340e64ed1fade721bf95ae2703c36714
Aleksey Arabchik <aleksey-090@mail.ru>
2023-04-25 18:27:10 +0300

    full

--- client/src/services/UserService.ts
+++
@@ -1,9 +0,0 @@
-import $api from "../http";
-import { IUser } from "../models/response/IUser";
-import { AxiosResponse } from "axios";
-
-export default class UserService {
-  static fetchUsers(): Promise<AxiosResponse<IUser[]>> {
-    return $api.get<IUser[]>("/users");
-  }
-}
