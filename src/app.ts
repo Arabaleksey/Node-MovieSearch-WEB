5c53e7684ae0e7a289ab357fe380b79d8516afea
Aleksey Arabchik <aleksey-090@mail.ru>
2023-04-26 21:57:34 +0300

    fix

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
 import express from "express";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
-import cors from "cors";
+let cors = require('cors')
 import router from "./router";
 import errorMiddleware from "./middlewares/error-middlewares";
 
 require("dotenv").config();
 const PORT = 5000;
 const app = express();
+
+
 
 app.use(express.json());
 app.use(cookieParser());
