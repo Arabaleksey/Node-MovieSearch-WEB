ab91440a19d2ecb5118e2363780c1f37d55e028c
Aleksey Arabchik <aleksey-090@mail.ru>
2023-05-01 14:54:36 +0300

    fix

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,16 @@
 const Router = require("express").Router;
-import tokenMiddlewares from "../middlewares/token-middlewares";
 import { userController } from "../controllers/user-controller";
+import tokenMiddlewares from "../middlewares/token-middlewares";
 
 const router = new Router();
 import { body } from "express-validator";
 
-
-router.post("/registration", 
-body('email').isEmail(),
-body('password').isLength({min:3, max:32}),
-userController.registration);
+router.post(
+  "/registration",
+  body("email").isEmail(),
+  body("password").isLength({ min: 3, max: 32 }),
+  userController.registration
+);
 router.post("/login", userController.login);
 router.post("/logout", userController.logout);
 router.get("/activate/:link", userController.activate);
