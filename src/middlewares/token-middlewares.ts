10a6330006718a8263035ba11df95a3280cd2249
Aleksey Arabchik <aleksey-090@mail.ru>
2023-05-01 14:34:26 +0300

    fix

--- src/middlewares/token-middlewares.ts
+++ src/middlewares/token-middlewares.ts
@@ -1,27 +1,27 @@
-let jwt = require('jsonwebtoken');
+// let jwt = require('jsonwebtoken');
 
-export default function (req:any, res:any, next:any) {
-  let token = req.headers['x-access-token'] || req.headers['authorization'];
-  if (token.startsWith('Bearer ')) {
-    token = token.slice(7, token.length);
-  }
+// export default function (req:any, res:any, next:any) {
+//   let token = req.headers['x-access-token'] || req.headers['authorization'];
+//   if (token.startsWith('Bearer ')) {
+//     token = token.slice(7, token.length);
+//   }
 
-  if (token) {
-    jwt.verify(token, process.env.JWT_ACCESS_SECRET, (err:any, decoded:any) => {
-      if (err) {
-        return res.json({
-          success: false,
-          message: 'Token is not valid'
-        });
-      } else {
-        req.decoded = decoded;
-        next();
-      }
-    });
-  } else {
-    return res.json({
-      success: false,
-      message: 'Auth token is not supplied'
-    });
-  }
-};
+//   if (token) {
+//     jwt.verify(token, process.env.JWT_ACCESS_SECRET, (err:any, decoded:any) => {
+//       if (err) {
+//         return res.json({
+//           success: false,
+//           message: 'Token is not valid'
+//         });
+//       } else {
+//         req.decoded = decoded;
+//         next();
+//       }
+//     });
+//   } else {
+//     return res.json({
+//       success: false,
+//       message: 'Auth token is not supplied'
+//     });
+//   }
+// };
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 const Router = require("express").Router;
-import tokenMiddlewares from "../middlewares/token-middlewares";
+// import tokenMiddlewares from "../middlewares/token-middlewares";
 import { userController } from "../controllers/user-controller";
 
 const router = new Router();
@@ -13,6 +13,6 @@ userController.registration);
 router.post("/login", userController.login);
 router.post("/logout", userController.logout);
 router.get("/activate/:link", userController.activate);
-router.get("/refresh", tokenMiddlewares, userController.refresh);
+router.get("/refresh", userController.refresh);
 
 export default router;
